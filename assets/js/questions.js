e4af56124260ec979d0448d13312eb6ae99c8d48
Sho.ayb <Sho.ayb@outlook.com>
2023-01-16 11:58:07 +0000

    first commit have some functions coded

---
+++ assets/js/questions.js
@@ -0,0 +1,23 @@
+/*
+
+
+This file contains the data to store all of the question objects 
+
+
+*/
+
+const questionsArr = [
+  {
+    question: "What data types can an array store ?",
+    choices: ["string", "numbers", "objects", "all of them"],
+    answer: "all of them",
+    isCorrect: false,
+    isFirst: true,
+  },
+  {
+    question: "Commonly used data types do not include ? ",
+    choices: ["string", "numbers", "objects", "alerts"],
+    answer: "alerts",
+    isCorrect: false,
+  },
+];
